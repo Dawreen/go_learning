0c4d789e3b426c1bfd9cbba8b654d182607bc21e
Dan <dntknwhtf@gmail.com>
2024-04-19 22:47:44 +0200

    My package with input

---
+++ go_learning.go
@@ -0,0 +1,10 @@
+package go_learning
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hell(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!\nDan is tring to learn GO...\nHe gave up on engliss some time ago", name)
+	return message
+}
