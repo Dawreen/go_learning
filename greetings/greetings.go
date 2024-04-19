6eb4d292df092577659bae7f6edd7c072b7ee73b
Dan <dntknwhtf@gmail.com>
2024-04-19 23:59:35 +0200

    Testing is easy and fun

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -15,6 +15,7 @@ func Hello(name string) (string, error) {
 
 	// If a name was received, return a greeting that embeds the name in a message.
 	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprint(randomFormat()) // Make tests FAIL
 	return message, nil
 }
 
