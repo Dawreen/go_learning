fad909ab9c3f1d5e28fc1b41e6f48e396469654b
Dan <dntknwhtf@gmail.com>
2024-04-19 23:31:02 +0200

    Random strings in a slice

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,8 +2,8 @@ package greetings
 
 import (
 	"errors"
-
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -14,6 +14,20 @@ func Hello(name string) (string, error) {
 	}
 
 	// If a name was received, return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!\nDan is tring to learn GO...\nHe gave up on engliss some time ago", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// random greeting
+func randomFormat() string {
+	// All the possible formats
+	formats := []string{
+		"Hi, %v. Welcome!\nDan is tring to learn GO...\nHe gave up on engliss some time ago",
+		"Hi, %v. Welcome!",
+		"Nice to see %v",
+		"Better luck next time! %v, please LEAVE!",
+	}
+
+	// Return a randomly selected message from the formats
+	return formats[rand.Intn(len(formats))]
 }
